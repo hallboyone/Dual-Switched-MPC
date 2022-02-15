dd2bef061a3f0d2f857e57455559a5a9742eeecc
Richard Hall <rah75@duke.edu>
2022-02-15 15:58:37 -0500

    Moved the parfor loops into the nodal operations.

--- matlab/functions/convexHullOfUnion.m
+++ matlab/functions/convexHullOfUnion.m
@@ -1,14 +1,21 @@
-function S = convexHullOfUnion(sets)
-    num_verticies = 0;
-    for i=1:numel(sets)
-        num_verticies = num_verticies + size(sets(i).V, 1);
+function S = convexHullOfUnion(agent, sets)
+    S = repmat(Polyhedron.emptySet(sets(1).Dim), 1, agent.nummodes);
+    for l = 1:numel(S)
+        num_verticies = 0;
+        for n=1:agent.graph.numnodes
+            if agent.graph.node(n).label == l
+                num_verticies = num_verticies + size(sets(n).V, 1);
+            end
+        end
+        V = zeros(num_verticies, sets(1).Dim);
+        idx = 1;
+        for n=1:agent.graph.numnodes
+            if agent.graph.node(n).label == l
+                V(idx:idx+size(sets(n).V, 1)-1, :) = sets(n).V;
+                idx = idx+size(sets(n).V, 1);
+            end
+        end
+        S(l) = Polyhedron(V);
+        S(l).minVRep();
     end
-    V = zeros(num_verticies, sets(1).Dim);
-    idx = 1;
-    for i=1:numel(sets)
-        V(idx:idx+size(sets(i).V, 1)-1, :) = sets(i).V;
-        idx = idx+size(sets(i).V, 1);
-    end
-    S = Polyhedron(V);
-    S.minVRep();
 end
